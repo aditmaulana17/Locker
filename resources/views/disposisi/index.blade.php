96271b2c624dc7fa9624e14156a8c3d3aa43ff91
unknown <aditmaulana2356@gmail.com>
2026-09-24 09:50:00 +0700

    update pada resources dan controller

--- resources/views/disposisi/index.blade.php
+++ resources/views/disposisi/index.blade.php
@@ -131,12 +131,6 @@
     $exportFilters = request()->query();
 
     $boardCollections = collect($boardDisposisis ?? []);
-    if ($boardCollections->isEmpty() && isset($disposisis)) {
-        $boardCollections = collect($disposisis)->groupBy(function ($item) use ($statusOptions) {
-            $status = strtolower(trim((string) ($item->status ?? 'menunggu')));
-            return in_array($status, array_keys($statusOptions), true) ? $status : 'menunggu';
-        });
-    }
 @endphp
 
 @push('styles')
